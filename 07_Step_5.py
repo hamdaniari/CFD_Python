760adfa74eb30b31c34083506e3bc42a67106087
Ari Hamdani <aryham04@gmail.com>
2020-08-31 17:33:12 +0900

    Update 07_Step_5.py

--- 07_Step_5.py
+++ 07_Step_5.py
@@ -32,4 +32,38 @@ u[int(0.5/dx):int(1/dx+1),int(0.5/dy):int(1/dy+1)] = 2
 fig = plt.figure(figsize=(11,7),dpi=100)
 ax = plt.gca(projection='3d')
 X, Y = np.meshgrid(x, y)
-surf = ax.plot_surface(X,Y,u[:],cmap=cm.viridis)
+Z = u
+surf = ax.plot_surface(X,Y,Z,cmap=cm.viridis)
+
+"""
+#for operation
+for n in range(nt+1):
+    un = u.copy()
+    row, col = u.shape
+    for i in range(1,col):
+        for j in range(1,row):
+            u[i,j] = (un[i,j])-c*dt/dx*(un[i,j]-(un[i-1,j]))-c*dt/dy*(un[i,j]-un[i,j-1])
+            
+            u[0,:] = 1
+            u[-1,:] = 1
+            u[:,0] = 1
+            u[:,-1] = 1
+            
+fig = plt.figure(figsize=(11, 7), dpi=100)
+ax = fig.gca(projection='3d')
+surf2 = ax.plot_surface(X, Y, u[:], cmap=cm.viridis)    
+"""
+
+#array operation
+for n in range(nt+1):
+    un = u.copy()
+    u[1:,1:] = (un[1:,1:]) - c*dt/dx*(un[1:,1:]-un[0:-1,1:]) - c*dt/dy*(un[1:,1:]-un[1:,0:-1])
+    
+    u[0,:] = 1
+    u[-1,:] = 1
+    u[:,0] = 1
+    u[:,-1] = 1
+    
+fig = plt.figure(figsize=(11, 7), dpi=100)
+ax = fig.gca(projection='3d')
+surf2 = ax.plot_surface(X, Y, u[:], cmap=cm.viridis)    
